7acdcbca1f75ef644e040eeb2cc27d2f5acdb059
zzuummaa <stepannnk@mail.ru>
2017-02-16 20:09:01 +0300

    Critical controller modul

---
+++ kk/kk_main.cpp
@@ -0,0 +1,62 @@
+//============================================================================
+// Name        : test1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "shm/redis.h"
+#include "structs.h"
+#include "GPIOClass.h"
+
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <string.h>
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+	dataService* dserv = new redisDataService();
+	dserv->connect();
+
+	GPIOClass* gpio4 = new GPIOClass("4"); //create new GPIO object to be attached to  GPIO4
+	GPIOClass* gpio2 = new GPIOClass("2"); //create new GPIO object to be attached to  GPIO17
+
+	gpio2->setdir_gpio("out"); //GPIO4 set to output
+	gpio4->setdir_gpio("in");
+
+	string inVal = "0";
+	string oldInVal = "0";
+	int impulsCount = 0;
+	int dataServiceTimer = 0;
+
+	while (1) {
+		gpio2->setval_gpio("1");
+		usleep(50000);
+		gpio2->setval_gpio("0");
+		usleep(50000);
+
+		gpio4->getval_gpio(inVal);
+		if ( inVal.compare(oldInVal) != 0 ) {
+			impulsCount++;
+			oldInVal = inVal;
+		}
+
+		dataServiceTimer += 100;
+
+		if (dataServiceTimer > 1000) {
+			tel_kk tel;
+			tel.impulsPerSec = impulsCount;
+
+			dserv->add(TYPE_KK, (char*)&tel, sizeof(tel_kk));
+
+			dataServiceTimer = 0;
+			impulsCount = 0;
+		}
+	}
+
+	return 0;
+}
